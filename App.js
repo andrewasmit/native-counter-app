57141143897e4c44bfa4cb6b9c89b433dfcc2120
andrew <andrewasmit@hotmail.com>
2023-07-03 21:08:16 -0500

    simple counter functions added

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
       <Text>This is Andrew's simple Counter-App</Text>
+      <Text>The Count: {count}</Text>
       <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
+        title="Increase the count"
+        onPress={() => setCount(count+1)}
+      />
+      <Button
+        title="Decrease the count"
+        onPress={() => setCount(count-1)}
       />
       <StatusBar style="auto" />
     </View>
